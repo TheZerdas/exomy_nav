5805bd7ec1ef0ea136cadbc1dfe80b0b5ea10696
TheZerdas <martin.hasal0@gmail.com>
2023-03-01 09:30:31 +0100

    start exomy

--- src/exomy/launch/exomy_modV2.launch.py
+++ src/exomy/launch/exomy_modV2.launch.py
@@ -13,8 +13,13 @@ namespace_ = 'exomy'
 def generate_launch_description():
     # Get paths to config files
     exomy_config = os.path.join(get_package_share_directory('exomy'),'exomy.yaml')
+    sim_config = os.path.join(get_package_share_directory('exomy_sim'),'rviz/simulation2.rviz')
+    world = os.path.join(get_package_share_directory('exomy_sim'), 'worlds/obstacles.world')
+    depth_param_config = os.path.join(get_package_share_directory('depthimage_to_laserscan'), 'cfg', 'param.yaml')
+    params_file = LaunchConfiguration('params_file')
     default_params_file = os.path.join(get_package_share_directory("exomy_sim"),
                                        'config', 'mapper_params_online_async.yaml')
+    #included_launch = launch_ros.actions.IncludeLaunchDescription(package='foo', launch='my_launch.py', arguments=[...])
 
     # Process the URDF file
     urdf_file = os.path.join(get_package_share_directory('exomy_sim'),'models/exomy_model/exomy_model.urdf')
@@ -71,6 +76,44 @@ def generate_launch_description():
         output='screen'
     )
 
+    rviz2 = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        # output='screen',
+        output={'stdout':'log'},
+        arguments=['-d', sim_config],
+        parameters=[{
+            'use_sim_time': use_sim_time,
+        }]
+    )
+
+    # gazebo = ExecuteProcess(
+    #     cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so', world],
+    #     output='screen')
+
+    # Spawn rover
+    # spawn_rover = Node(
+    #     package='gazebo_ros',
+    #     executable='spawn_entity.py',
+    #     name='spawn_entity',
+    #     namespace='',
+    #     arguments=['-entity',
+    #                'exomy',
+    #                '-x', '1.5', '-y', '1', '-z', '2',
+    #                '-file', urdf_file,
+    #                '-reference_frame', 'world']
+    # )
+
+    depth2scan = Node(
+            package='depthimage_to_laserscan',
+            node_executable='depthimage_to_laserscan_node',
+            node_name='depthimage_to_laserscan_node',
+            remappings=[('depth','/camera/depth/image_raw'),
+                        ('depth_camera_info', '/camera/depth/camera_info'),
+                        ('scan','/scan')],
+            parameters=[depth_param_config])
+
     return LaunchDescription([
         # Set env var to print messages colored. The ANSI color codes will appear in a log.
         SetEnvironmentVariable('RCUTILS_COLORIZED_OUTPUT', '1'),
@@ -84,6 +127,11 @@ def generate_launch_description():
         robot,
         joint_command_node,
         robot_state_publisher,
+        # depth2scan, #pridat az bude realsense
+        rviz2,
+        # gazebo,
+        # spawn_rover,
+        depth2scan,
         motors,
         
     ])
